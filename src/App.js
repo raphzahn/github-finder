c93fdad49d7f69e30c023228c288d088d2b5c2c6
raphzahn <rz271994@gmail.com>
2021-04-12 21:48:34 +0200

    fetch Users form Github API

--- src/App.js
+++ src/App.js
@@ -1,18 +1,33 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Navbar from './components/layout/Navbar';
 import Users from './components/users/Users';
+import axios from 'axios'
 import './App.css';
 
-const App = () => {
+class App extends Component {
+  state = {
+    users: [],
+    loading: false,
+  }
+
+  async componentDidMount() {
+    this.setState({loading: true});
+    const res = await axios.get('https://api.github.com/users');
+    console.log(res.data);
+    this.setState({ users: res.data, loading: false });
+  }
+  render(){
     return (
       <div className="App">
         <Navbar />
         <div className="container">
-          <Users />
+          <Users loading={this.state.loading} users={this.state.users} />
         </div>
         
       </div>
     );
+  }
+
 }
 
 export default App;
--- src/components/users/Users.js
+++ src/components/users/Users.js
@@ -1,39 +1,16 @@
-import React from 'react';
+import React, { Component } from 'react';
 import UserItem from './UserItem';
 
-const Users = (state) => {
-    state = {
-        users: [
-            {
-                id: 1,
-                login: 'mojombo',
-                avatar_url: 'https://avatars.githubusercontent.com/u/1?v=4',
-                html_url: 'https://github.com/mojombo',
-            },
-            {
-                id: 2,
-                login: 'defunkt',
-                avatar_url: 'https://avatars.githubusercontent.com/u/2?v=4',
-                html_url: 'https://github.com/defunkt',
-            },
-            {
-                id: 3,
-                login: 'pjhyett',
-                avatar_url: 'https://avatars.githubusercontent.com/u/3?v=4',
-                html_url: 'https://github.com/pjhyett',
-            }
-        ]
+class Users extends Component {
+    render() {
+        return (
+            <div className="user-cards">
+                { this.props.users.map(user => (
+                    <UserItem className="" key={user.id} user={user}/>
+                ))}
+            </div>
+        )
     }
-
-
-    return (
-        <div className="user-cards">
-            {state.users.map(user => (
-                <UserItem className="" key={user.id} user={user}/>
-            ))}
-        </div>
-    )
-
 }
 
 export default Users;
