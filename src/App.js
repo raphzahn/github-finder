e1872b14f834c4373f1d413d5f11c46c08363f5e
Raphael Zahner <r.zahner@voegeli.ch>
2022-02-11 18:21:56 +0100

    add NotFound Page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
-import React, { Fragment } from 'react';
+import React from 'react';
 import { BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+
 import Navbar from './components/layout/Navbar';
 import Alert from './components/layout/Alert';
+
+import User from './components/users/User';
+
 import About from './components/pages/About';
 import Home from './components/pages/Home';
-import User from './components/users/User';
+import NotFound from './components/pages/NotFound';
+
 import GithubState from './context/github/GithubState';
 import AlertState from './context/alert/AlertState';
+
 import './App.css';
 
 const App = () => {
@@ -23,6 +29,7 @@ const App = () => {
               <Route exact path='/' component={Home}/>
               <Route exact path='/about' component={About}/>
               <Route exact path='/user/:username' component={User}/>
+              <Route component={NotFound} />
             </Switch>
             
           </div>
