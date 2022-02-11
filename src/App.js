a38e26f803f84599986a7fe762241f7b5dd3946e
Raphael Zahner <r.zahner@voegeli.ch>
2022-02-11 11:05:46 +0100

    add User Page

--- src/App.js
+++ src/App.js
@@ -5,12 +5,14 @@ import Users from './components/users/Users';
 import Search from './components/users/Search';
 import Alert from './components/layout/Alert';
 import About from './components/pages/About';
+import User from './components/users/User';
 import axios from 'axios'
 import './App.css';
 
 class App extends Component {
   state = {
     users: [],
+    user: {},
     loading: false,
     alert: null,
   }
@@ -19,6 +21,12 @@ class App extends Component {
     this.setState({loading : true})
     const res = await axios.get(`https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secred=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
     this.setState({ users: res.data.items, loading: false });
+  }
+
+  getUser = async (username) => {
+    this.setState({loading : true})
+    const res = await axios.get(`https://api.github.com/users/${username}?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secred=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
+    this.setState({ user: res.data, loading: false });
   }
 
   clearUsers = () => {
@@ -33,7 +41,7 @@ class App extends Component {
   };
 
   render(){
-    const {users, loading} = this.state
+    const {users, loading, user} = this.state
 
     return (
       <Router>
@@ -49,6 +57,9 @@ class App extends Component {
                 </Fragment>
               )} />
               <Route exact path='/about' component={About}/>
+              <Route exact path='/user/:username' render={props => (
+                <User { ...props } getUser={this.getUser} user={user} loading={loading}/>
+              )}/>
             </Switch>
             
           </div>
---
+++ src/components/users/User.js
@@ -0,0 +1,59 @@
+import React, { Component, Fragment } from 'react'
+import Spinner from '../layout/Spinner';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+
+export class User extends Component {
+    componentDidMount() {
+        this.props.getUser(this.props.match.params.username)
+    }
+
+    static propTypes = {
+        loading: PropTypes.bool,
+        user: PropTypes.object.isRequired,
+        getUser: PropTypes.func.isRequired,
+    }
+
+    render() {
+        const {name, avatar_url, location, company, bio, blog, login, html_url, followers, following, public_repos, public_gists, hireable} = this.props.user;
+        const {loading} = this.props;
+        if(loading) return <Spinner />
+        return (
+            <Fragment>
+                <Link to="/" className="btn btn-light">Back to Search</Link>
+                Hireable: {' '}
+                {hireable ? (<i className="fas fa-check text-success" />) : (<i className="fas fa-times-circle text-danger" />) }
+                <div className="card grid-2">
+                    <div className="all-center">
+                        <img src={avatar_url} alt="" className="round-img" style={{width:'150px'}}/>
+                        <h1>{name}</h1>
+                        <p>{location}</p>
+                    </div>
+                    <div>
+                        {bio && (<Fragment><h3>Bio</h3><p>{bio}</p></Fragment>)}
+                        <a href={html_url} className="btn btn-dark my-1">Visit Github Profil</a>
+                        <ul>
+                            <li>
+                                {login && (<Fragment><b>Username: </b>{login}</Fragment>)}
+                            </li>
+                            <li>
+                                {blog && (<Fragment><b>Company: </b>{company}</Fragment>)}
+                            </li>
+                            <li>
+                                {blog && (<Fragment><b>Website: </b>{blog}</Fragment>)}
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+                <div className="card text-center">
+                    <div className="badge badge-primary">Followers: {followers}</div>
+                    <div className="badge badge-success">Following: {following}</div>
+                    <div className="badge badge-ligth">Public Repos: {public_repos}</div>
+                    <div className="badge badge-dark">Public Gists: {public_gists}</div>
+                </div>
+            </Fragment>
+        )
+    }
+}
+
+export default User
